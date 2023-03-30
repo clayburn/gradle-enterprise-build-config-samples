3a44f2b01162d1eab7b55f29d956f1b66f91a8e9
Clay Johnson <cjohnson@gradle.com>
2023-03-30 08:52:42 -0500

    Add specific values

--- gradle-enterprise-conventions-maven-extension/extension/src/main/java/com/example/CustomGradleEnterpriseConfig.java
+++ gradle-enterprise-conventions-maven-extension/extension/src/main/java/com/example/CustomGradleEnterpriseConfig.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import com.gradle.maven.extension.api.GradleEnterpriseApi;
+import com.gradle.maven.extension.api.cache.BuildCacheApi;
 import com.gradle.maven.extension.api.scan.BuildScanApi;
 import org.apache.maven.execution.MavenSession;
 
@@ -9,9 +10,14 @@ final class CustomGradleEnterpriseConfig {
     void extendBuildScan(GradleEnterpriseApi api, MavenSession session) {
         BuildScanApi buildScan = api.getBuildScan();
 
-        // Use `buildScan` to create commonly needed tags, values, and links. For example:
-        // buildScan.tag("Sample Tag");
-        // buildScan.value("Sample Name", "Sample Value");
-        // buildScan.link("Sample Link", "https://example.com");
+        buildScan.value("Parallel enabled", Boolean.toString(session.isParallel()));
+        buildScan.value("Module count", Integer.toString(session.getProjects().size()));
+        session.getRequest().getActiveProfiles().forEach(profile -> buildScan.value("Active profile", profile));
+
+        BuildCacheApi buildCache = api.getBuildCache();
+        buildScan.value("Local cache enabled", Boolean.toString(buildCache.getLocal().isEnabled()));
+        buildScan.value("Remote cache enabled", Boolean.toString(buildCache.getRemote().isEnabled()));
+
+        buildScan.value("Convention Extension Version", getClass().getPackage().getImplementationVersion());
     }
 }
